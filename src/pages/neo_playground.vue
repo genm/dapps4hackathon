9357d3fce916653cf75a7f945ff17972722da666
Taisei Imada <genm1023@gmail.com>
2018-08-25 13:56:59 +0900

    Fix: neo_playground Add: Alphive logo

--- src/pages/neo_playground.vue
+++ src/pages/neo_playground.vue
@@ -4,7 +4,6 @@
 
   </div>
 
-
 </template>
 
 <script>
@@ -17,23 +16,6 @@ export default {
   created() {
     //ここで色々試してみて
     console.log(Neon.CONST.NEO_NETWORK.MAIN);
-    const MYNET = "MyNet"
-    const privnet_protcol = require('../protocol.json');
-
-    const privateNet = new rpc.Network(privnet_protcol, MYNET);
-
-    const sb = Neon.create.scriptBuilder();
-
-    sb.emitAppCall('5b7074e873973a6ed3708862f219a6fbf4d1c411', 'name');
-
-    rpc.Query.invokeScript(sb.str).execute('http://127.0.0.1:30333')
-      .then(res => {
-        console.log(res);
-
-      })
-      .catch(res => {
-        console.log(res);
-      })
 
   },
 
---
+++ src/pages/registration.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        dapps4hackathon
+      </h1>
+      <h2 class="subtitle">
+        NEO hackathon 2018
+      </h2>
+      <div class="links">
+        <nuxt-link to="neo_playground">
+          <p
+            class="button--green">Neo Playground</p>
+        </nuxt-link>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+
+
+</script>
